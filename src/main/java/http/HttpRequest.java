70cf1f3725be6dc966f39178cda5f61db1860b0f
americanomin <americanomin@gmail.com>
2018-03-19 23:28:13 +0900

    chapter-5 HttpRequest class을 p158과 똑같이 변경하다

--- src/main/java/http/HttpRequest.java
+++ src/main/java/http/HttpRequest.java
@@ -1,5 +1,7 @@
 package http;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import util.HttpRequestUtils;
 import util.IOUtils;
 
@@ -8,33 +10,38 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class HttpRequest {
-    private String path;
+    private static final Logger log = LoggerFactory.getLogger(HttpRequest.class);
     private String method;
-    private HashMap<String, String> header = new HashMap<>();
+    private String path;
+    private HashMap<String, String> headers = new HashMap<>();
     private Map<String, String> params;
 
-    public HttpRequest(InputStream in) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
-        String[] RequestLine = br.readLine().split(" ");
-        method = RequestLine[0];
-        String[] UrlToken =  RequestLine[1].split("\\?");
-        path = UrlToken[0];
-
-        int contentLength = 0;
-        String line = br.readLine();
-        while (line != null && !line.equals("")) {
-            header.put(HttpRequestUtils.parseHeader(line).getKey(), HttpRequestUtils.parseHeader(line).getValue());
-            if (line.contains("Content-Length")) {
-                contentLength = getContentLength(line);
+    public HttpRequest(InputStream in) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            String line = br.readLine();
+            if(line == null){
+                return;
             }
-            line = br.readLine();
-        }
 
-        if(method.equals("GET")){
-            params = HttpRequestUtils.parseQueryString(UrlToken[1]);
-        }else if(method.equals("POST")){
-            String body = IOUtils.readData(br, contentLength);
-            params = HttpRequestUtils.parseQueryString(body);
+            processRequestLine(line);
+
+            line = br.readLine();
+
+            while (line != null && !line.equals("")) {
+                log.debug("headers : {}", line);
+                String[] tokens = line.split(":");
+
+                headers.put(tokens[0].trim(), tokens[1].trim());
+                line = br.readLine();
+            }
+
+            if(method.equals("POST")){
+                String body = IOUtils.readData(br, Integer.parseInt(headers.get("Content-Length")));
+                params = HttpRequestUtils.parseQueryString(body);
+            }
+        }catch (IOException io){
+            log.error(io.getMessage());
         }
     }
 
@@ -51,11 +58,31 @@ public class HttpRequest {
     }
 
     public String getHeader(String key) {
-        return header.get(key);
+        return headers.get(key);
     }
 
     private int getContentLength(String line) {
         String[] headerTokens = line.split(":");
         return Integer.parseInt(headerTokens[1].trim());
     }
+
+    public void processRequestLine(String requestLine) {
+        log.debug("request line : {}", requestLine);
+        String[] tokens = requestLine.split(" ");
+        method = tokens[0];
+
+        if("POST".equals(method)){
+            path = tokens[1];
+            return;
+        }
+
+        int index = tokens[1].indexOf("?");
+
+        if(index == -1){
+            path = tokens[1];
+        }else{
+            path = tokens[1].substring(0, index);
+            params = HttpRequestUtils.parseQueryString(tokens[1].substring(index+1));
+        }
+     }
 }
